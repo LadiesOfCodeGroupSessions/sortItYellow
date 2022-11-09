ccbcf71275b81496c7b7ac0e316505dc4fc4242e
Elisa Evans <elisa.evans@outlook.com>
2022-11-09 19:18:46 +0000

    test new rack

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ class Balls:
     def __init__(self):
         self.total = 60
 
+class Rack:
+
+    def __init__(self):
+        self = []
--- test_sortIt.py
+++ test_sortIt.py
@@ -1,5 +1,10 @@
 from main import Balls
+from main import Rack
 
 def test_totalBalls():
     balls = Balls()
     assert balls.total == 60
+
+def test_new_rack():
+    rack = Rack()
+    assert rack == []
